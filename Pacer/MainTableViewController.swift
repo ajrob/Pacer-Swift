e3746173ac0f7418c29154d886d685b1c0f5ed27
ajrob <ajrob.dev@gmail.com>
2015-03-03 20:56:32 -0700

    Fresh restart on the tableViewController.

--- Pacer/MainTableViewController.swift
+++ Pacer/MainTableViewController.swift
@@ -20,20 +20,13 @@ class MainTableViewController: UITableViewController, UIPickerViewDelegate, UIPi
     let kAttributePickerCellID = "attributePicker" // the cell containing the picker
     let kOtherCellID = "otherCell"
     
-    // keep track of which rows have date cells
-    let kAttributeStartRow = 1
-    let kAttributeEndRow   = 2
-    
-    let paceKey = "paceKey"
-    let paceValueKey = "paceValueKey"
-    
     var arrayBaseSixty = [Int]()
-    
-    var dataArray: [[String: AnyObject]] = []
     
     let testArray = ["ksljd", "sldkjf", "jajaja", "bwoeklw"]
     
     var pickerIndexPath: NSIndexPath?
+    
+    var dataArray = [[String:String]]()
     
     var pickerCellRowHeight: CGFloat = 216
 
@@ -66,7 +59,6 @@ class MainTableViewController: UITableViewController, UIPickerViewDelegate, UIPi
     // MARK: - Picker view data source
     // returns the number of 'columns' to display.
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        
         return 1
     }
     
@@ -78,70 +70,12 @@ class MainTableViewController: UITableViewController, UIPickerViewDelegate, UIPi
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
         return testArray[row]
     }
-    
-    // MARK: - Utilities
-    
-    /*! Determines if the given indexPath has a cell below it with a UIDatePicker.
-    
-    @param indexPath The indexPath to check if its cell has a UIDatePicker below it.
-    */
-    func hasPickerForIndexPath(indexPath: NSIndexPath) -> Bool {
-        var hasPicker = false
-        
-        let targetedRow = indexPath.row + 1
-        
-        let checkPickerCell = tableView.cellForRowAtIndexPath(NSIndexPath(forRow: targetedRow, inSection: 0))
-        let checkPicker = checkPickerCell?.viewWithTag(kPickerTag)
-        
-        hasPicker = checkPicker != nil
-        return hasPicker
-    }
-    
-    /*! Updates the UIPickerView's value to match with the date of the cell above it.
-    !!!!! NOT FUNCTIONAL RIGHT NOW!!!!!!!!
-    */
-    func updatePicker() {
-        if pickerIndexPath != nil {
-            let associatedPickerCell = tableView.cellForRowAtIndexPath(pickerIndexPath!)
-            
-            if let targetedPicker = associatedPickerCell?.viewWithTag(kPickerTag) as UIPickerView? {
-                // We found a UIPickerView in this cell, so update it's value
-                let itemData = dataArray[self.pickerIndexPath!.row - 1]
-//                targetedPicker.selectRow(<#row: Int#>, inComponent: <#Int#>, animated: <#Bool#>)
-            }
-        }
-    }
-    
-    /*! Determines if the given indexPath points to a cell that contains the UIPickerView.
-    
-    @param indexPath The indexPath to check if it represents a cell with the UIPickerView.
-    */
-    func indexPathHasPicker(indexPath: NSIndexPath) -> Bool {
-        return (hasInlinePicker() && pickerIndexPath?.row == indexPath.row)
-    }
-    
-    /*! Determines if the given indexPath points to a cell that contains the attributes
-    
-    @param indexPath The indexPath to check if it represents attributes cell.
-    */
-    func indexPathHasDate(indexPath: NSIndexPath) -> Bool {
-        var hasAttribute = false
-        
-        if (indexPath.row == kAttributeStartRow) || (indexPath.row == kAttributeEndRow || (hasInlinePicker() && (indexPath.row == kAttributeEndRow + 1))) {
-            hasAttribute = true
-        }
-        return hasAttribute
-    }
 
     // MARK: - UITableViewDataSource
 
     private struct Storyboard {
         static let AttributeCellResuseIdentifier = "attributeCell"
         static let AttributePickerCellReuseIdentifier = "attributePicker"
-    }
-    
-    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return (indexPathHasPicker(indexPath) ? pickerCellRowHeight : tableView.rowHeight)
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -153,120 +87,10 @@ class MainTableViewController: UITableViewController, UIPickerViewDelegate, UIPi
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        if hasInlinePicker() {
-            // We have a picker, so allow for it in the number of rows in this section
-            var numRows = dataArray.count
-            return ++numRows
-        }
         
         return dataArray.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        var cell: UITableViewCell?
-        
-        var cellID = kOtherCellID
-        
-        if indexPathHasPicker(indexPath) {
-            // the indexPath is the one containing the inline date picker
-            cellID = kAttributePickerCellID     // the current/opened date picker cell
-        } else if indexPathHasDate(indexPath) {
-            // the indexPath is one that contains the date information
-            cellID = kAttributeCellID       // the start/end date cells
-        }
-        
-        cell = tableView.dequeueReusableCellWithIdentifier(cellID) as? UITableViewCell
-        
-        if indexPath.row == 0 {
-            // we decide here that first cell in the table is not selectable (it's just an indicator)
-            cell?.selectionStyle = .None;
-        }
-        
-        // if we have a date picker open whose cell is above the cell we want to update,
-        // then we have one more cell than the model allows
-        //
-        var modelRow = indexPath.row
-        if (pickerIndexPath != nil && pickerIndexPath?.row <= indexPath.row) {
-            modelRow--
-        }
-        
-        let itemData = dataArray[modelRow]
-        
-        if cellID == kAttributeCellID {
-            // we have either start or end date cells, populate their date field
-            //
-            cell?.textLabel?.text = itemData[paceKey] as? String
-            cell?.detailTextLabel?.text = itemData[paceValueKey] as? String
-        } else if cellID == kOtherCellID {
-            // this cell is a non-date cell, just assign it's text label
-            //
-            cell?.textLabel?.text = itemData[paceKey] as? String
-        }
-        
-        return cell!
-    }
-    
-    /* Adds or removes a UIPickerView cell below the given indexPath
-    @param indexPath The indexxPath to reveal the UIPickerView */
-    func togglePickerForSelectedIndexPath(indexPath: NSIndexPath) {
-        tableView.beginUpdates()
-        
-        let indexPaths = [NSIndexPath(forRow: indexPath.row + 1, inSection: 0)]
-        
-        // Check if 'indexPath' has an attached picker below it
-        if hasPickerForIndexPath(indexPath) {
-            // Found a picker below it, so remove it
-            tableView.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: .Bottom)
-        } else {
-            // Didn't find a picker below it, so we should insert it
-            tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: .Top)
-        }
-        tableView.endUpdates()
-    }
-    
-    
-    /* Reveals the picker inline for the given indexPath, called by "didSelectRowAtIndexPath".
-    @param indexPath The indexPath to reveal the UIPickerView */
-    func displayInlinePickerForRow(indexPath: NSIndexPath) {
-        // Display the picker inline with the table content
-        tableView.beginUpdates()
-        
-        var before = false // Indicates if the picker is below "indexPath" to help determine which row to reveal
-        if (hasInlinePicker()) {
-            before = pickerIndexPath?.row < indexPath.row
-        }
-        
-        var sameCellClicked = (pickerIndexPath?.row == indexPath.row + 1)
-        
-        // Remove any picker cell if it exists
-        if self.hasInlinePicker() {
-            tableView.deleteRowsAtIndexPaths([NSIndexPath(forRow: pickerIndexPath!.row, inSection: 0)], withRowAnimation: .Top)
-            pickerIndexPath = nil;
-        }
-        
-        if !sameCellClicked {
-            // Hide the old picker and display the new one
-            let rowToReveal = (before ? indexPath.row - 1 : indexPath.row)
-            let indexPathToReveal = NSIndexPath(forRow: rowToReveal, inSection: 0)
-            
-            self.togglePickerForSelectedIndexPath(indexPathToReveal)
-            pickerIndexPath = NSIndexPath(forRow: indexPathToReveal.row + 1, inSection: 0)
-        }
-        
-        // Always deselect the row containing the attribute ('Pace', 'Distance', or 'Duration')
-        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        
-        tableView.endUpdates()
-        
-        // Inform our picker of the current attribute value to match the current cell
-        updatePicker()
-    }
-    
-    /* Determines if the UITableViewController has a UIPickerView in any of its cells */
-    func hasInlinePicker() -> Bool {
-        return pickerIndexPath != nil
-    }
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -315,12 +139,7 @@ class MainTableViewController: UITableViewController, UIPickerViewDelegate, UIPi
     // MARK: UITableViewDelegate
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let cell = tableView.cellForRowAtIndexPath(indexPath)
-        if cell?.reuseIdentifier == kAttributeCellID {
-            displayInlinePickerForRow(indexPath)
-        } else {
-            tableView.deselectRowAtIndexPath(indexPath, animated: true)
-        }
+        
     }
 
 }
